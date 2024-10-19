00da02486e6286fb91c0a46a37b86de5fcdb8912
Michael Stokesbary <mike@stokesbary.me>
2024-10-19 11:52:24 -0700

    Did some cleanup for event editing.

--- src/UserGroupSite.Client/Components/Admin/Pages/CreateEditEvent.razor.cs
+++ src/UserGroupSite.Client/Components/Admin/Pages/CreateEditEvent.razor.cs
@@ -1,9 +1,7 @@
 using System.Net.Http.Json;
-using System.Text.Json;
 using System.Text.RegularExpressions;
 using Blazored.FluentValidation;
 using Blazored.Toast.Services;
-using Markdig;
 using UserGroupSite.Client.Services;
 using SharedConstants = UserGroupSite.Shared.Models.Constants;
 
@@ -11,14 +9,7 @@ namespace UserGroupSite.Client.Components.Admin.Pages;
 
 public partial class CreateEditEvent : ComponentBase, IDisposable
 {
-    class PersistentData
-    {
-        public List<CategoryDto> Categories { get; set; }
-        public List<UserDto> Users { get; set; }
-    }
     private FluentValidationValidator _fluentValidationValidator = default!;
-    
-    private string _preview => Markdown.ToHtml(Dto?.Description ?? "");
 
     [SupplyParameterFromForm] private EventDto Dto { get; set; } = new();
     [SupplyParameterFromForm] private string ButtonAction { get; set; } = default!;
@@ -28,8 +19,6 @@ public partial class CreateEditEvent : ComponentBase, IDisposable
     private string _messageResult = default!;
     private bool _eventSaved = false;
     private bool _isEditing = false;
-    
-    private List<string> _errorMessages { get; set; } = new();
     
     private List<CategoryDto> _categories = new();
     private List<UserDto> _users = new();
@@ -68,39 +57,6 @@ public partial class CreateEditEvent : ComponentBase, IDisposable
                 Dto = foundInState ? restoredEventData! : await EventService.GetEventByIdAsync(EventId.Value);
             }
         }
-        
-        // if (HttpClient.BaseAddress is not null)
-        // {
-        //     _categories = await HttpClient.GetFromJsonAsync<List<CategoryDto>>($"{SharedConstants.CategoryApiUrl}");
-        //     _users = await HttpClient.GetFromJsonAsync<List<UserDto>>($"{SharedConstants.UserApiUrl}");
-        //     Dto.CategoryId = _categories.FirstOrDefault().CategoryId;
-        //     Dto.SpeakerId = _users.FirstOrDefault().UserId;
-        //     
-        //     if (EventId.HasValue)
-        //     {
-        //         _isEditing = true;
-        //         if (Dto.EventId == default)
-        //         {
-        //             Dto = await HttpClient.GetFromJsonAsync<EventDto>($"{SharedConstants.EventApiUrl}/{EventId}");
-        //         }
-        //     }
-        // }
-        
-        // _categories = await DbContext.Categories.ToListAsync();
-        // _users = await DbContext.Users.ToListAsync();
-        //
-        // if (EventId.HasValue)
-        // {
-        //     _isEditing = true;
-        //     if (Dto.EventId == default)
-        //     {
-        //         var speakingEvent = await DbContext.SpeakingEvents.SingleOrDefaultAsync(se => se.Id == EventId);
-        //         if (speakingEvent is not null)
-        //         {
-        //             Dto = speakingEvent.ToDto();
-        //         }
-        //     }
-        // }
     }
 
     private async Task DeleteEvent(int id)
@@ -129,60 +85,16 @@ public partial class CreateEditEvent : ComponentBase, IDisposable
             {
                 result = await HttpClient.PostAsJsonAsync($"{SharedConstants.EventApiUrl}/create", Dto);
             }
-            
-            var jsonString = await result.Content.ReadAsStringAsync();
-            JsonSerializerOptions options = new()
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
 
             if (result.IsSuccessStatusCode)
             {
-                // Dto = JsonSerializer.Deserialize<EventDto>(jsonString, options);
+                // ToastService.ShowSuccess("The Event has been saved off correctly.");
                 NavigationManager.NavigateTo("/admin/manageevents");
             }
             else
             {
                 ToastService.ShowError("Unable to save the event, please correct the errors and try again.");
             }
-            // if (Dto.EventId > 0) // update the event
-            // {
-            //     Logger.LogInformation("Attempting to update event");
-            //     var speakingEvent = await DbContext.SpeakingEvents.SingleOrDefaultAsync(s => s.Id == Dto.EventId);
-            //     speakingEvent.FromDto(Dto);
-            //     
-            //     var saveResult = await DbContext.SaveChangesAsync();
-            //     if (saveResult > 0)
-            //     {
-            //         NavigationManager.NavigateTo($"/Admin/ManageEvents/");
-            //     }
-            //     else
-            //     {
-            //         _messageResult = "There was an error updating the category, please try again.";
-            //         _eventSaved = false;
-            //     }
-            // }
-            // else // need to insert
-            // {
-            //     Logger.LogInformation("Attempting to create event");
-            //     
-            //     var newEvent = new SpeakingEvent();
-            //
-            //     newEvent.FromDto(Dto);
-            //
-            //     DbContext.SpeakingEvents.Add(newEvent);
-            //
-            //     var saveResult = await DbContext.SaveChangesAsync();
-            //     if (saveResult > 0)
-            //     {
-            //         NavigationManager.NavigateTo("/Admin/ManageEvents");
-            //     }
-            //     else
-            //     {
-            //         _messageResult = "There was an error creating the category, please try again.";
-            //         _eventSaved = false;
-            //     }
-            // }
         }
     }
     
@@ -208,14 +120,6 @@ public partial class CreateEditEvent : ComponentBase, IDisposable
 
     private Task PersistData()
     {
-        var persistedData = new PersistentData()
-        {
-            Categories = _categories,
-            Users = _users,
-        };
-        // PersistentComponentState.PersistAsJson("persistedData", persistedData);
-        
-        // PersistentComponentState.PersistAsJson("someValue", "Just a test");
         PersistentComponentState.PersistAsJson(nameof(_categories), _categories);
         PersistentComponentState.PersistAsJson(nameof(_users), _users);
         if (_isEditing)
